5d997af7a70a236604cb7dd5b98b6a51ae11c63c
ihor_ostapovych <thor.ostapovych@danfoss.com>
2022-06-30 17:36:36 +0300

    fix: module dep sum

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -17,12 +18,66 @@ Feel free to drop gorilla.mux if you want and use any other solution available.
 main function reads host/port from env just for an example, flavor it following your taste
 */
 
+func NameHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	_, err := fmt.Fprintf(w, "Hello, %v!", vars["PARAM"])
+	if err != nil {
+		_ = fmt.Errorf("[NameHandler] Error: %w", err)
+	}
+}
+
+func BadHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+}
+
+func DataHandler(w http.ResponseWriter, r *http.Request) {
+	b, err := io.ReadAll(r.Body)
+	if err != nil {
+		_ = fmt.Errorf("[DataHandler] Error: %w", err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	_, err = io.WriteString(w, fmt.Sprintf("I got message:\n%v", string(b)))
+	if err != nil {
+		_ = fmt.Errorf("[DataHandler] Error: %w", err)
+	}
+}
+
+func HeadersHandler(w http.ResponseWriter, r *http.Request) {
+	h := r.Header
+	a, err := strconv.Atoi(h.Get("a"))
+	if err != nil {
+		_ = fmt.Errorf("[DataHandler] Error: %w", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	b, err := strconv.Atoi(h.Get("b"))
+	if err != nil {
+		_ = fmt.Errorf("[DataHandler] Error: %w", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	sum := a + b
+
+	fmt.Println(fmt.Sprintf("%d", sum))
+
+	w.Header().Add("a+b", fmt.Sprintf("%d", sum))
+	w.WriteHeader(http.StatusOK)
+}
+
 // Start /** Starts the web server listener on given host and port.
 func Start(host string, port int) {
-	router := mux.NewRouter()
+	r := mux.NewRouter()
+
+	r.HandleFunc("/name/{PARAM}", NameHandler).Methods("GET")
+	r.HandleFunc("/bad", BadHandler).Methods("GET")
+	r.HandleFunc("/data", DataHandler).Methods("POST")
+	r.HandleFunc("/headers", HeadersHandler).Methods("POST")
 
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
-	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
+	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), r); err != nil {
 		log.Fatal(err)
 	}
 }
