799314d1e876ae56ed7f25406ccea1ff5731b79b
Uriy3 <99492882+Uriy3@users.noreply.github.com>
2022-07-14 21:43:03 +0300

    Update muxing.go

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -11,6 +12,52 @@ import (
 	"github.com/gorilla/mux"
 )
 
+func handleNameParam(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	param := params["PARAM"]
+	fmt.Fprintf(w, "Hello, "+param+"!")
+
+	return
+}
+
+func handleBadParam(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+
+	return
+}
+
+func handdleBodyParam(w http.ResponseWriter, r *http.Request) {
+	data, err := io.ReadAll(r.Body)
+
+	if err != nil {
+		errors.New("something went wrong with http response")
+		return
+	}
+
+	fmt.Fprintf(w, "I got message:\n"+string(data))
+
+	return
+}
+
+func handleHeadersParam(w http.ResponseWriter, r *http.Request) {
+	hdrA := r.Header.Get("a")
+	hdrB := r.Header.Get("b")
+
+	if hdrA == "" || hdrB == "" {
+		errors.New("No available header!")
+		return
+	}
+
+	res1, e1 := strconv.Atoi(hdrA)
+	res2, e2 := strconv.Atoi(hdrB)
+
+	if e1 == nil && e2 == nil {
+		w.Header().Set("a+b", strconv.Itoa(res1+res2))
+	}
+
+	return
+}
+
 /**
 Please note Start functions is a placeholder for you to start your own solution.
 Feel free to drop gorilla.mux if you want and use any other solution available.
@@ -20,10 +67,12 @@ main function reads host/port from env just for an example, flavor it following 
 // Start /** Starts the web server listener on given host and port.
 func Start(host string, port int) {
 	router := mux.NewRouter()
-	router.HandleFunc("/name/{param}", GetParam).Methods("GET")
-	router.HandleFunc("/bad", GetErr).Methods("GET")
-	router.HandleFunc("/data", PostParam).Methods("POST")
-	router.HandleFunc("/headers", PostHeaders).Methods("POST")
+
+	router.HandleFunc("/name/{PARAM}", handleNameParam).Methods(http.MethodGet)
+	router.HandleFunc("/bad", handleBadParam).Methods(http.MethodGet)
+	router.HandleFunc("/data", handdleBodyParam).Methods(http.MethodPost)
+	router.HandleFunc("/headers", handleHeadersParam).Methods(http.MethodPost)
+
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
 		log.Fatal(err)
@@ -38,29 +87,4 @@ func main() {
 		port = 8081
 	}
 	Start(host, port)
-}
-func GetParam(resp http.ResponseWriter, req *http.Request) {
-	vars := mux.Vars(req)
-	resp.WriteHeader(http.StatusOK)
-	fmt.Fprintf(resp, "Hello, %v!", vars["param"])
-}
-
-func GetErr(resp http.ResponseWriter, req *http.Request) {
-	resp.WriteHeader(http.StatusInternalServerError)
-}
-
-func PostParam(resp http.ResponseWriter, req *http.Request) {
-	resp.WriteHeader(http.StatusOK)
-	body, _ := io.ReadAll(req.Body)
-	fmt.Fprintf(resp, "I got message:\n%v", string(body))
-}
-
-func PostHeaders(resp http.ResponseWriter, req *http.Request) {
-	a, b := req.Header.Get("a"), req.Header.Get("b")
-	var intA, intB int
-	intA, _ = strconv.Atoi(a)
-	intB, _ = strconv.Atoi(b)
-	r := intA + intB
-	resp.Header().Set("a+b", strconv.Itoa(r))
-	resp.WriteHeader(http.StatusOK)
 }
